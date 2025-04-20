f378db0002331b6d6a265ec74dc495ac0b8edeff
Jadavivek <126766074+Jadavivek@users.noreply.github.com>
2025-04-20 18:55:54 +0530

    Update ab.py

--- ab.py
+++ ab.py
@@ -1,10 +1,1 @@
-# from celery import Celery
-# app = Celery(
-#     'tasks',
-#     broker='redis://localhost:6379/0',
-#     backend='redis://localhost:6379/0'
-# )
-import redis
 
-r = redis.Redis(host='localhost', port=6379)
-print(r.ping())  # Should print True
